2b1a7934108476c023e5c4dcda322de133007a22
ITzTravelInTime <pietrocaruso97@yahoo.it>
2021-06-27 13:34:18 +0200

    Version 1.1.0
    -Added a protocol called `CommandExecutor` to manage Extensions of the `Command` class
    -Added generic implementation for the `Command.run` function, called `Command.genericRun`, so it can be used by extensions of `Command` for compliance with `CommandExecutor`
    -Made the `Command` compliant with the `CommandExecutor` protocol

--- Sources/Command/Command.swift
+++ Sources/Command/Command.swift
@@ -2,8 +2,22 @@ import Foundation
 //import TINURecovery
 
 #if os(macOS)
+
+public protocol CommandExecutor {
+    static func start(cmd: String, args: [String]) -> Command.Handle?
+    static func run(cmd : String, args : [String]?) -> Command.Result?
+    static func run(cmd : String) -> Command.Result?
+    static func getOut(cmd: String) -> String?
+    init()
+}
+
 ///This class manages the usage of executables and terminal commands using `Process` objects.
-public final class Command{
+public final class Command: CommandExecutor{
+    
+    ///Initializer for compliance with the `CommandExecutor` protocol
+    public required init(){
+        //Litterally does nothing since this class is used more like a namespe and so it doesn't contain any stored values to intialize
+    }
     
     //initalizers are kept internal to avoid having the library user messing too with the execution state or results.
     
@@ -115,28 +129,61 @@ public final class Command{
     }
     
     /**
+     This function manages an exectuin from start to execution finish, for a gived `CommandExecutor` type object
+        
+        - Parameters:
+            - executor: Instance of the `CommandExecutor` type class that is going to run the execution
+            - cmd: The path to the executable to launch in order to perform the command, or the String with the command/script to execute (see the description of the `args` parameter to learn more).
+            - args: The arguments for the specified executable, if nil the `cmd` parameter will be run as a terminal command/script using the sh shell.
+     
+        - Returns: The `Command.Result` object obtained from the execution
+     
+        - Precondition:
+            - This will suspend the thread it's running on, avoid running this from the main thread or the app/program will stop responding!
+            - This function requres sandboxing to be dissbled unless it's run by passing the path for an executable embedded into the current bundle into the `cmd` argument and the `args` argument is not nil
+     */
+    public class func genericRun<T: CommandExecutor>(_ executor: T, cmd : String, args : [String]? = nil ) -> Command.Result? {
+        //Can't do T.run because that's recursive with the current implementation
+        if let cargs = args{
+            return Command.result(from: T.start(cmd: cmd, args: cargs))
+        }
+        
+        assert(!cmd.isEmpty, "The process needs a path to an executable to execute!")
+        //assert(!Sandbox.isEnabled, "The app sandbox should be disabled to perform this operation!!")
+        return Command.result(from: T.start(cmd: "/bin/sh", args: ["-c", cmd]))
+    }
+    
+    /**
      This function manages a `Process` object from start to execution finish
         
         - Parameters:
             - cmd: The path to the executable to launch in order to perform the command, or the command to execute (see the description of the `args` parameter to learn more).
             - args: The arguments for the specified executable, if nil the `cmd` parameter will be run as a terminal command using the sh shell.
      
-        - Returns: The `Result` object obtained from the execution of the `Process` object
+        - Returns: The `Command.Result` object obtained from the execution of the `Process` object
      
         - Precondition:
             - This will suspend the thread it's running on, avoid running this from the main thread or the app/program will stop responding!
             - This function requres sandboxing to be dissbled unless it's run by passing the path for an executable embedded into the current bundle into the `cmd` argument and the `args` argument is not nil
-                    
-     
      */
-    public class func run(cmd : String, args : [String]? = nil ) -> Result? {
-        if let cargs = args{
-            return result(from: start(cmd: cmd, args: cargs))
-        }
+    public static func run(cmd: String, args: [String]?) -> Result? {
+        return genericRun(Command(), cmd: cmd, args: args)
+    }
+    
+    /**
+     This function manages a `Process` object from start to execution finish, from a shell command/script string
         
-        assert(!cmd.isEmpty, "The process needs a path to an executable to execute!")
-        //assert(!Sandbox.isEnabled, "The app sandbox should be disabled to perform this operation!!")
-        return result(from: start(cmd: "/bin/sh", args: ["-c", cmd]))
+        - Parameters:
+            - cmd: The string with the command/script to execute using the sh shell.
+     
+        - Returns: The `Command.Result` object obtained from the execution of the `Process` object
+     
+        - Precondition:
+            - This will suspend the thread it's running on, avoid running this from the main thread or the app/program will stop responding!
+            - This function requres sandboxing to be dissbled unless it's run by passing the path for an executable embedded into the current bundle into the `cmd` argument and the `args` argument is not nil
+     */
+    public static func run(cmd: String) -> Result? {
+        return genericRun(Command(), cmd: cmd, args: nil)
     }
     
     /**
@@ -153,7 +200,7 @@ public final class Command{
             - This function requres sandboxing to be dissbled.
      
      */
-    public class func getOut(cmd: String, isErr: Bool = false) -> String?{
+    public class func getOut(cmd: String, isErr: Bool) -> String?{
         //ths function runs a command on the sh shell and it does return the output or the error produced
         
         guard let res = run(cmd: cmd) else { return nil }
@@ -171,6 +218,23 @@ public final class Command{
         }
         
         return rett
+    }
+    
+    /**
+     Executes a command from start to finish using the sh shell and then returns either the standard output or the standard error of it's execution.
+        
+        - Parameters:
+            - cmd: The command to perform using the sh shell.
+     
+        - Returns: The `[String]` object obtained from the execution of the `Process` object, which is it's standard output. If both the standard error and the standard output are need use the `Command.run` function with the `arg` parameter setted to nil instead.
+     
+        - Precondition:
+            - This will suspend the thread it's running on, avoid running this from the main thread or the app/program will stop responding!
+            - This function requres sandboxing to be dissbled.
+     
+     */
+    public static func getOut(cmd: String) -> String? {
+        return getOut(cmd: cmd, isErr: false)
     }
     
     /**
